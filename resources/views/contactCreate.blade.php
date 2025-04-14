017ce478fc636cbe6e3fa8deace4a2514e3f606a
Panha <turtoisecool@gmail.com>
2025-04-14 23:48:34 +0700

    Fixed /addcontact

--- resources/views/contactCreate.blade.php
+++ resources/views/contactCreate.blade.php
@@ -1,5 +1,7 @@
 @php
     $faker = \Faker\Factory::create();
+    $departments = App\Models\Departments::all();
+    $positions = App\Models\Positions::all();
 @endphp
 
 <x-layout>
@@ -26,7 +28,8 @@
                         <div class="sm:col-span-2">
                             <label for="first_name" class="block text-sm/6 font-medium text-gray-900">First name</label>
                             <div class="mt-2">
-                                <input type="text" name="first_name" id="first_name" autocomplete="given-name" value="Nipanha"
+                                <input type="text" name="first_name" id="first_name" autocomplete="given-name"
+                                    value="Nipanha"
                                     class="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6">
                             </div>
                         </div>
@@ -34,26 +37,67 @@
                         <div class="sm:col-span-2">
                             <label for="last_name" class="block text-sm/6 font-medium text-gray-900">Last name</label>
                             <div class="mt-2">
-                                <input type="text" name="last_name" id="last_name" autocomplete="family-name" value="Sameth"
+                                <input type="text" name="last_name" id="last_name" autocomplete="family-name"
+                                    value="Sameth"
                                     class="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6">
                             </div>
                         </div>
 
                         <div class="sm:col-span-2 sm:col-start-1">
-                            <label for="department_name" class="block text-sm/6 font-medium text-gray-900">Department</label>
+                            <label for="department_id" class="block text-sm/6 font-medium text-gray-900">Departments</label>
+                            <div class="mt-2 grid grid-cols-1">
+                                <select id="department_id" name="department_id" autocomplete="department_id-name"
+                                    class="col-start-1 row-start-1 w-full appearance-none rounded-md bg-white py-1.5 pr-8 pl-3 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6">
+                                    @foreach ($departments as $department)
+                                        <option value="{{ $department->id }}">{{ $department->name }}</option>
+                                    @endforeach
+                                </select>
+                                <svg class="pointer-events-none col-start-1 row-start-1 mr-2 size-5 self-center justify-self-end text-gray-500 sm:size-4"
+                                    viewBox="0 0 16 16" fill="currentColor" aria-hidden="true" data-slot="icon">
+                                    <path fill-rule="evenodd"
+                                        d="M4.22 6.22a.75.75 0 0 1 1.06 0L8 8.94l2.72-2.72a.75.75 0 1 1 1.06 1.06l-3.25 3.25a.75.75 0 0 1-1.06 0L4.22 7.28a.75.75 0 0 1 0-1.06Z"
+                                        clip-rule="evenodd" />
+                                </svg>
+                            </div>
+                        </div>
+                        
+                        <div class="sm:col-span-2">
+                            <label for="position_id" class="block text-sm/6 font-medium text-gray-900">Positions</label>
+                            <div class="mt-2 grid grid-cols-1">
+                                <select id="position_id" name="position_id" autocomplete="position_id"
+                                    class="col-start-1 row-start-1 w-full appearance-none rounded-md bg-white py-1.5 pr-8 pl-3 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6">
+                                    @foreach ($positions as $position)
+                                        <option value="{{ $position->id }}">{{ $position->title }}</option>
+                                    @endforeach
+                                </select>
+                                <svg class="pointer-events-none col-start-1 row-start-1 mr-2 size-5 self-center justify-self-end text-gray-500 sm:size-4"
+                                    viewBox="0 0 16 16" fill="currentColor" aria-hidden="true" data-slot="icon">
+                                    <path fill-rule="evenodd"
+                                        d="M4.22 6.22a.75.75 0 0 1 1.06 0L8 8.94l2.72-2.72a.75.75 0 1 1 1.06 1.06l-3.25 3.25a.75.75 0 0 1-1.06 0L4.22 7.28a.75.75 0 0 1 0-1.06Z"
+                                        clip-rule="evenodd" />
+                                </svg>
+                            </div>
+                        </div>
+
+                        {{-- <div class="sm:col-span-2 sm:col-start-1">
+                            <label for="department_name"
+                                class="block text-sm/6 font-medium text-gray-900">Department</label>
                             <div class="mt-2">
-                                <input type="text" name="department_name" id="department_name" autocomplete="address-level2" value="IT"
+                                <input type="text" name="department_name" id="department_name"
+                                    autocomplete="address-level2" value="IT"
                                     class="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6">
                             </div>
                         </div>
 
                         <div class="sm:col-span-2">
-                            <label for="position_name" class="block text-sm/6 font-medium text-gray-900">Position</label>
+                            <label for="position_name"
+                                class="block text-sm/6 font-medium text-gray-900">Position</label>
                             <div class="mt-2">
-                                <input type="text" name="position_name" id="position_name" autocomplete="address-level1" value="Web Developer"
+                                <input type="text" name="position_name" id="position_name" autocomplete="address-level1"
+                                    value="Web Developer"
                                     class="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6">
                             </div>
-                        </div>
+                        </div> --}}
 
                         <div class="sm:col-span-4">
                             <label for="email" class="block text-sm/6 font-medium text-gray-900">Email address</label>
@@ -64,9 +108,11 @@
                         </div>
 
                         <div class="sm:col-span-2 sm:col-start-1">
-                            <label for="phone_number" class="block text-sm/6 font-medium text-gray-900">Phone Number</label>
+                            <label for="phone_number" class="block text-sm/6 font-medium text-gray-900">Phone
+                                Number</label>
                             <div class="mt-2">
-                                <input type="text" name="phone_number" id="phone_number" autocomplete="address-level1" value="012-345-678"
+                                <input type="text" name="phone_number" id="phone_number" autocomplete="address-level1"
+                                    value="012-345-678"
                                     class="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6">
                             </div>
                         </div>
@@ -74,7 +120,8 @@
                         <div class="sm:col-span-2">
                             <label for="hired_date" class="block text-sm/6 font-medium text-gray-900">Hired Date</label>
                             <div class="mt-2">
-                                <input type="text" name="hired_date" id="hired_date" autocomplete="address-level1" value="2025-01-01"
+                                <input type="text" name="hired_date" id="hired_date" autocomplete="address-level1"
+                                    value="2025-01-01"
                                     class="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6">
                             </div>
                         </div>
--- routes/web.php
+++ routes/web.php
@@ -18,12 +18,14 @@ Route::post('/addcontact', function() {
     $contact->last_name = request('last_name');
     $contact->email = request('email');
 
-    $department = \App\Models\Departments::where('name', request('department_name'))->first();
+    // $department = \App\Models\Departments::where('name', request('department_name'))->first();
+    // $contact->department_id = $department->getAttribute('id');
+    $contact->department_id = request('department_id');
 
-    $contact->department_id = $department->getAttribute('id');
+    // $position = \App\Models\Positions::where('title', request('position_name'))->first();
+    // $contact->position_id = $position->getAttribute('id');;
 
-    $position = \App\Models\Positions::where('title', request('position_name'))->first();
-    $contact->position_id = $position->getAttribute('id');;
+    $contact->position_id = request('position_id');
 
     $contact->hire_date = request('hired_date');
     $contact->phone = request('phone_number');
