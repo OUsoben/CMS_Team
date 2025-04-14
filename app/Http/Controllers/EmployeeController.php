d22a2e7242297126a9a8142d06600dc8ee75b65e
Panha <turtoisecool@gmail.com>
2025-04-15 00:25:20 +0700

    with last commit

---
+++ app/Http/Controllers/EmployeeController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class EmployeeController extends Controller
+{
+    function index() {
+        return view('contactCreate');
+    }
+
+    function store(Request $request) {
+        $contact = new \App\Models\Employees();
+        $contact->first_name = $request->input('first_name');
+        $contact->last_name = $request->input('last_name');
+        $contact->gender = $request->input('gender');
+        $contact->email = $request->input('email');
+        $contact->department_id = $request->input('department_id');
+        $contact->position_id = $request->input('position_id');
+        $contact->hire_date = $request->input('hired_date');
+        $contact->phone = $request->input('phone_number');
+
+        $contact->save();
+
+        return redirect('/contactlist')->with('success', 'Contact added successfully!');
+    }
+}
